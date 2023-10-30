8d7e5ad9f4fa84f337a8d10c2facfb3e63db8051
Oso32 <osito7932@gmail.com>
2023-10-30 17:13:31 -0300

    hice mi primer ejercicio

---
+++ actividad-23-10-23.py
@@ -0,0 +1,11 @@
+dia = 23
+mes = 11
+año = 2023
+print("La fecha de hoy es",dia,"/",mes,"/",año)
+print(f"La fecha de hoy es {dia}/{mes}/{año}")
+dia = str(dia)
+mes = str(mes)
+año = str(año)
+print("La fecha de hoy es "+dia+"/"+mes+"/"+año)
+mes = mes ^ 2
+
